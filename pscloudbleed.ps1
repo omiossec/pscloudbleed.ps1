dda5d01056d843e8f62277c719de8655739bd16e
Olivier Miossec <oliviermiossec@hotmail.fr>
2017-02-26 19:54:06 +0100

    Initial Commit

---
+++ pscloudbleed.ps1
@@ -0,0 +1,165 @@
+   <#
+.SYNOPSIS 
+    this script will extract domain from chrome and Ie history and test this domains to the cloudbleed domain list
+.EXAMPLE
+    .\pscloudbleed.ps1 [-username string]
+    
+
+.NOTES
+    the cloudbleed domain list in provided. If you want the latest one simply delete the file sorted_unique_cf.txt file
+    this script will download it again from pirate github 
+    https://github.com/pirate/sites-using-cloudflare
+
+    Author       : Olivier Miossec <olivier@omiossec.work>
+#> 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    function get-cloudFlareTld
+    {
+        # this function download the cloudbleed domain list from github if it doesn't exist in the current directory
+        # Please read https://github.com/pirate/sites-using-cloudflare 
+
+        if (!(test-path -Path "sorted_unique_cf.txt"))
+        {
+                    
+            $githubrawdata = "https://raw.githubusercontent.com/pirate/sites-using-cloudflare/master/sorted_unique_cf.txt"
+
+            Invoke-WebRequest -Uri $githubrawdata -UseBasicParsing -OutFile sorted_unique_cf.txt
+
+        }
+
+
+    }
+
+
+
+# this 2 functions is a modification of get-browserdata.ps1 from rvrsh3ll https://github.com/rvrsh3ll/Misc-Powershell-Scripts
+
+    function Get-ChromeHistory {
+
+        $chromeUrl = @()
+
+       
+      
+        $Path = "$Env:systemdrive\Users\$UserName\AppData\Local\Google\Chrome\User Data\Default\History"
+        if (-not (Test-Path -Path $Path)) {
+            Write-Verbose "[!] Could not find Chrome History for username: $UserName"
+        }
+        $Regex = '(htt(p|s))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+        $Value = Get-Content -Path "$Env:systemdrive\Users\$UserName\AppData\Local\Google\Chrome\User Data\Default\History"|Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
+        
+        $Value | ForEach-Object {
+            $Key = $_
+            
+            if ($Key -match $Search){
+
+                
+                $tmp =  ([System.Uri]$_).Host 
+                $tmp = $tmp -replace '^www\.'
+
+                $chromeUrl += $tmp
+                
+
+            }
+        }      
+
+        return   $chromeUrl
+    }
+
+
+    # this function is a modification of get-browserdata.ps1 from rvrsh3ll https://github.com/rvrsh3ll/Misc-Powershell-Scripts
+
+    function Get-InternetExplorerHistory {
+        #https://crucialsecurityblog.harris.com/2011/03/14/typedurls-part-1/
+        $iEUrl = @()
+        $Null = New-PSDrive -Name HKU -PSProvider Registry -Root HKEY_USERS
+        $Paths = Get-ChildItem 'HKU:\' -ErrorAction SilentlyContinue | Where-Object { $_.Name -match 'S-1-5-21-[0-9]+-[0-9]+-[0-9]+-[0-9]+$' }
+
+        ForEach($Path in $Paths) {
+
+            $User = ([System.Security.Principal.SecurityIdentifier] $Path.PSChildName).Translate( [System.Security.Principal.NTAccount]) | Select-Object -ExpandProperty Value
+
+            $Path = $Path | Select-Object -ExpandProperty PSPath
+
+            $UserPath = "$Path\Software\Microsoft\Internet Explorer\TypedURLs"
+            if (-not (Test-Path -Path $UserPath)) {
+                Write-Verbose "[!] Could not find IE History for SID: $Path"
+            }
+            else {
+                Get-Item -Path $UserPath -ErrorAction SilentlyContinue | ForEach-Object {
+                    $Key = $_
+                    $Key.GetValueNames() | ForEach-Object {
+                        $Value = $Key.GetValue($_)
+                        if ($Value -match $Search) {
+                                $tmp =  ([System.Uri]$Value).Host 
+                                $tmp = $tmp -replace '^www\.'
+                                $IEUrl += $tmp
+                            }
+                        }
+                    }
+                }
+            }
+
+            return $iEUrl
+        }
+    
+
+
+
+
+
+
+    if (!$UserName) {
+        $UserName = "$ENV:USERNAME"
+    }
+
+
+
+# Get the full path of the domain file. NET Object do not work with relative path in powershell
+
+$Path = split-path -parent $MyInvocation.MyCommand.Definition 
+$Path += "\sorted_unique_cf.txt"
+
+
+
+
+
+
+# create an  array with distinct value from chrome and Ie, it's an UNION 
+
+$chromeUrl = Get-ChromeHistory
+$ieUrl = get-InternetExplorerHistory
+
+$VisitedUrl = Compare-Object $chromeUrl  $ieUrl  -IncludeEqual -PassThru
+
+ 
+ if (!(test-path -Path "sorted_unique_cf.txt"))
+        {
+            get-cloudFlareTld
+        }
+
+
+$reader = [System.IO.File]::OpenText($path)
+try {
+    for() {
+        $line = $reader.ReadLine() 
+        if ($line -eq $null) { break }
+        # process the line
+         
+        if ($VisitedUrl -contains $line)
+        {
+            Write-Host  $line
+        }
+    }
+}
+finally {
+    $reader.Close()
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+**PowerShell cloudbleed IE/Chrome check**
+
+
+This script use [https://github.com/pirate/sites-using-cloudflare]() and check IE or Chrome history on the local machine for cloudbleed affected domain
+
+
+To understand Cloudbleed you should read this [https://blog.cloudflare.com/incident-report-on-memory-leak-caused-by-cloudflare-parser-bug/]()
+
+In short, a bug in Cloudflare proxy leaked sensitive data between 2016-09 22 and 2017-02-18. This proxy service is used by many web sites and web services, from the small one to the Bigest.
+Session data as been leaked, you should log out session from this website. 
+In addition you should also try to reset password from this sites. 
+
+
+this script is written in PowerShell, it is an alpha version. 
+There is some change needed to improve performence
+
+I use [System.IO.File]::OpenText instead of get-content (it take too long and using NET object require only few seconds)
